d49b3e8ea8db551789fce058b57d56a99028799f
Jamie Naioti <Jamie.Naioti@razorfish.com>
2015-02-18 13:20:51 -0600

    made some animation and style edits to the views and nav states

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -7,6 +7,7 @@ $(document).ready(function()
 ///// INIT vars
 var nav = $("nav"),
     navItems = [];
+selectedNavButton = "empty";
 
 ///// INIT build
 initBuild = function() {
@@ -56,30 +57,80 @@ NavItem.prototype.addEventListeners = function(){
 }
 
 onOver = function(evt) {
-  TweenLite.to($(".headlines", this), 0.35, {height:150, width:350, ease:Power4.easeOut});
-  // TweenLite.to(this, 0.55, {height:250, ease:Power4.easeOut})
-}
-
-onOut = function(evt) {
-  TweenLite.to($(".headlines", this), 0.35, {height:85, ease:Power4.easeInOut});
+  if( $(this).is(".clicked") ) {
+    // console.log("this is the active button buddy")
+  } else {
+    var headlines = $(".headlines", this);
+    TweenLite.to(headlines, 0.35, {height:130, width:450, backgroundColor:"#ecfafb", ease:Power4.easeOut});
+    TweenLite.to($("h2", headlines), 0.45, {scale:1.45, color:"#6c4962", transformOrigin:"0% 0%", paddingBottom:"8px", ease:Power4.easeOut});
+    TweenLite.to($("h3", headlines), 0.35, {scale:1.25, paddingLeft:"12px", transformOrigin:"0% 0%", ease:Power3.easeOut});
+    TweenLite.to($(".icon", this), 0.35, {scale:1.5, transformOrigin:"100% 0%", ease:Power3.easeOut});
+  }
   
 }
 
+onOut = function(evt) {
+  if( $(this).is(".clicked") ) {
+    // console.log("this is the active button buddy")
+  } else {
+    var headlines = $(".headlines", this);
+    TweenLite.to(headlines, 0.25, {height:85, width:290, backgroundColor:"#ffffff", ease:Power2.easeIn});
+    TweenLite.to($("h2", headlines), 0.25, {scale:1, color:"#495a6c", transformOrigin:"0% 0%", paddingBottom:"0px", ease:Power2.easeIn});
+    TweenLite.to($("h3", headlines), 0.25, {scale:1, paddingLeft:"10px", transformOrigin:"0% 0%", ease:Power2.easeIn});
+    TweenLite.to($(".icon", this), 0.25, {scale:0.75, transformOrigin:"100% 0%", ease:Power3.easeIn});
+  }
+
+}
+
 onClick = function(evt) {
-  console.log(this)
+  var activeBtn = $( "li.clicked"),
+      headlines = $(".headlines", activeBtn);
+
+  // transition out the selected button 
+  TweenLite.to(headlines, 0.25, {height:85, width:290, backgroundColor:"#ffffff", ease:Power2.easeIn});
+  TweenLite.to($("h2", headlines), 0.25, {scale:1, color:"#495a6c", transformOrigin:"0% 0%", paddingBottom:"0px", ease:Power2.easeIn});
+  TweenLite.to($("h3", headlines), 0.25, {scale:1, color:"#495a6c", transformOrigin:"0% 0%", ease:Power2.easeIn});
+  TweenLite.to($(".icon", activeBtn), 0.25, {scale:0.75, transformOrigin:"100% 0%", ease:Power3.easeIn});
   //remove clicked class from all buttons in menu
-  $( ".clicked", "ul").removeClass("clicked");
+  activeBtn.removeClass("clicked");
+
   //animate all the nav elements 
   // TweenLite.to($(".headlines", ".main_nav"), 0.35, {width:120, ease:Power4.easeInOut})
-  $(this).addClass("clicked");
-  TweenLite.to($(".headlines", this), 0.25, {backgroundColor:"#ecfafb"});
+  selectedNavButton = $(this);
+  selectedNavButton.addClass("clicked");
+  selectSection(this.id.substring(7));
+  // console.log("selectedNavButton: ", selectedNavButton)
+  TweenLite.to($(".headlines", selectedNavButton), 0.25, {height:"95px", width:"500px", backgroundColor:"#495a6c"});
+  TweenLite.to($("h2", selectedNavButton), 0.25, {scale:1, color:"#FFFFFF", transformOrigin:"0% 0%", paddingBottom:"0px", ease:Power2.easeIn});
+  TweenLite.to($("h3", selectedNavButton), 0.25, {scale:1, color:"#FFFFFF", transformOrigin:"0% 0%", ease:Power2.easeIn});
+  TweenLite.to($(".icon", this), 0.25, {scale:1, transformOrigin:"100% 0%", ease:Power3.easeIn});
 }
 
 
 NavItem.prototype.animateIn = function(delay) {
   TweenLite.to(this.obj, 0.55, {marginTop:0, ease:Power4.easeOut, delay:delay})
-  TweenLite.to(this.headlineBox, 0.75, {width:350, ease:Power4.easeInOut, delay:delay})
+  TweenLite.to(this.headlineBox, 0.75, {width:290, ease:Power4.easeInOut, delay:delay})
   TweenLite.to(this.icon, 0.75, {drawSVG:"50% 50%", scale:1, autoAlpha:1, ease:Power4.easeInOut, delay:delay})
   // TweenLite.to(this.headline, 0.75, {opacity:1, ease:Power4.easeInOut, delay:delay + 0.25});
   // TweenLite.to(this.subhead, 0.75, {opacity:1, ease:Power4.easeInOut, delay:delay + 0.75});
 };
+
+selectSection = function(sectionNum) {
+  var content = $("#content")
+  TweenLite.to(content, 0.75, {width:"100%", ease:Power4.easeOut});
+  TweenLite.to($(".view", content), 0.75, {height:"25px", backgroundColor:"#acacac", ease:Power4.easeOut});
+
+  // get the active view
+  TweenLite.to($("#view" + sectionNum, content), 1, {height:"680px", backgroundColor:"#775260", ease:Power4.easeOut})
+  // switch(sectionNum) {
+  //   case "1" : 
+  //     TweenLite.to(content, 1.25, {marginTop:"0px", ease:Power4.easeOut});
+  //   case "2" :
+  //     TweenLite.to(content, 1.25, {marginTop:"-101px", ease:Power4.easeOut});
+  //   case "3" :
+  //     TweenLite.to(content, 1.25, {marginTop:"-201px", ease:Power4.easeOut});
+  //   case "4" :
+  //     TweenLite.to(content, 1.25, {marginTop:"-301px", ease:Power4.easeOut});
+
+  // }
+}
